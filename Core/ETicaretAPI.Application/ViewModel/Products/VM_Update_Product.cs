a68364b23d632e1d39fe659f4a9d9e76ae1ff1dd
tunahanozen <92800666+tunacim@users.noreply.github.com>
2023-08-10 19:53:26 +0300

    Test Get Update Delete Put
    Test amaçlı Product için create put delete  modelleri eklendi.
    controller da  Get Post Put Delete  eklendi .

---
+++ Core/ETicaretAPI.Application/ViewModel/Products/VM_Update_Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ETicaretAPI.Application.ViewModel.Products
+{
+	public class VM_Update_Product
+	{
+		public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Stock { get; set; }
+
+        public float Price { get; set; }
+    }
+}
+
--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Application.ViewModel.Products;
 using ETicaretAPI.Domain.Entities;
 using ETicaretAPI.Persistence.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -28,28 +30,60 @@ namespace ETicaretAPI.API.Controllers
 
         public ProductsController(
             IProductReadRepository productReadRepository,
-            IProductWriteRepository productWriteRepository,
-            IOrderWriteRepository orderWriteRepository,
-            ICustomerWriteRepository customerWriteRepository,
-            IOrderReadRepository orderReadRepository)
+            IProductWriteRepository productWriteRepository)
+     
         {
             _productReadRepository = productReadRepository;
             _productWriteRepository = productWriteRepository;
-            _orderWriteRepository = orderWriteRepository;
-            _customerWriteRepository = customerWriteRepository;
-            _orderReadRepository = orderReadRepository;
+           
         }
 
         [HttpGet]
-        public async   Task  <IActionResult>Get()
+        public async Task <IActionResult>Get()
         {
 
-            return Ok("merhaba");
-            
-            
+            return Ok(_productReadRepository.GetAll(false));
         }
-       
-      
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            return Ok(_productReadRepository.GetByIdAsync(id,false));
+        }
+
+
+        [HttpPost]
+        public async Task <IActionResult> Post(VM_Create_Product model )
+        {
+             await _productWriteRepository.AddAsync(new()
+            {
+                Name=model.Name,
+                Stock=model.Stock,
+                Price=model.Price
+            });
+            await _productWriteRepository.SaveAsync();
+            return StatusCode((int)HttpStatusCode.Created);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Put(VM_Update_Product model) 
+        {
+            Product product = await _productReadRepository.GetByIdAsync(model.Id);
+            product.Stock = model.Stock;
+            product.Name = model.Name;
+            product.Price = model.Price;
+
+           await _productWriteRepository.SaveAsync();
+
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _productWriteRepository.RemoveAsync(id);
+
+            await _productWriteRepository.SaveAsync();
+            return Ok();
+        }
+
     }
 }
 
